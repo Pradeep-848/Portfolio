93da5bf3863860367baba188b0643a2210dca9b8
Pradeep M <125256248+Pradeep-848@users.noreply.github.com>
2025-03-31 19:35:44 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,8 @@
 import React, { useRef } from 'react';
 import '../styles/Projects.css';
 import weather from '../assets/projects/weather.png'
-import vintage from '../assets/projects/Vintage.jpg'
+//import vintage from '../assets/projects/Vintage.jpg'
+import smartbot from '../assets/projects/Smartbot.jpg'
 import games from '../assets/projects/games.png'
 import aztracon from '../assets/projects/aztracon.jpg'
 import dbquest from '../assets/projects/dbquest.jpg'
@@ -9,7 +10,8 @@ import dbquest from '../assets/projects/dbquest.jpg'
 const projectsData = [
   { id: 1, title: 'Alzamil', description: 'Developed mobile applications for real-time ERP maintenance.', img: aztracon, link: 'https://play.google.com/store/search?q=al+zamil+app&c=apps&hl=en'  },
   { id: 2, title: 'DBQuest', description: 'Developed mobile applications for an IT company, enabling employees and administrators to manage attendance, worksheets, and various other tasks efficiently.', img: dbquest  },
-  { id: 3, title: 'Vintage Spare Parts', description: 'Vintage Spare Parts specializes in sourcing, restoring, and providing authentic, high-quality spare parts for classic vehicles.', img: vintage },
+  { id: 3, title: 'Smartbot', description: 'ChatGpt Clone', img: smartbot  },
+  //{ id: 3, title: 'Vintage Spare Parts', description: 'Vintage Spare Parts specializes in sourcing, restoring, and providing authentic, high-quality spare parts for classic vehicles.', img: vintage },
   { id: 4, title: 'Weather App', description: 'Weather app providing current weather and forecast with cities-based search', img: weather, link: 'https://weather-app-smoky-chi.vercel.app/' },
   { id: 5, title: 'Games', description: 'Small Works', img: games, link: 'https://games-rho-pearl.vercel.app/'  }, 
 ];
