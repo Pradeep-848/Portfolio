63e9cb0e124f6e57a6dd3a7123beb50c4cd01b27
Pradeep M <125256248+Pradeep-848@users.noreply.github.com>
2025-04-30 20:49:50 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,20 +1,22 @@
 import React, { useRef } from 'react';
 import '../styles/Projects.css';
-import weather from '../assets/projects/weather.png'
-//import vintage from '../assets/projects/Vintage.jpg'
-import smartbot from '../assets/projects/Smartbot.jpg'
-import stsweb from '../assets/projects/STSWEB.png'
-//import games from '../assets/projects/games.png'
 import aztracon from '../assets/projects/aztracon.jpg'
 import dbquest from '../assets/projects/dbquest.jpg'
+import stsapp from '../assets/projects/STSAPP.jpg'
+import stsweb from '../assets/projects/STSWEB.png'
+import smartbot from '../assets/projects/Smartbot.jpg'
+import weather from '../assets/projects/weather.png'
+//import vintage from '../assets/projects/Vintage.jpg'
+//import games from '../assets/projects/games.png'
 
 const projectsData = [
   { id: 1, title: 'Alzamil', description: 'Developed mobile applications for real-time ERP maintenance.', img: aztracon, link: 'https://play.google.com/store/search?q=al+zamil+app&c=apps&hl=en'  },
   { id: 2, title: 'DBQuest', description: 'Developed mobile applications for an IT company, enabling employees and administrators to manage attendance, worksheets, and various other tasks efficiently.', img: dbquest  },
-  { id: 3, title: 'Smartbot', description: 'ChatGpt Clone', img: smartbot, link: 'https://drive.google.com/file/d/1WMQ4aKaYmEA2VbteG001lX684j0GIL7Q/view?usp=drive_link'  },
-  //{ id: 3, title: 'Vintage Spare Parts', description: 'Vintage Spare Parts specializes in sourcing, restoring, and providing authentic, high-quality spare parts for classic vehicles.', img: vintage },
+  { id: 3, title: 'STS Pavillion (APP)', description: 'Wedding Pavillion App', img: stsapp },
   { id: 4, title: 'STS Pavillion (WEB)', description: 'Multi-screen interface with elegant UI for Weddings, Receptions, and Private Events', img: stsweb, link: 'https://stsmahal-web.vercel.app/' },
-  { id: 5, title: 'Weather App', description: 'Weather app providing current weather and forecast with cities-based search', img: weather, link: 'https://weather-app-smoky-chi.vercel.app/' },
+  { id: 5, title: 'Smartbot', description: 'ChatGpt Clone', img: smartbot, link: 'https://drive.google.com/file/d/1WMQ4aKaYmEA2VbteG001lX684j0GIL7Q/view?usp=drive_link'  },
+  { id: 6, title: 'Weather App', description: 'Weather app providing current weather and forecast with cities-based search', img: weather, link: 'https://weather-app-smoky-chi.vercel.app/' },
+  //{ id: 3, title: 'Vintage Spare Parts', description: 'Vintage Spare Parts specializes in sourcing, restoring, and providing authentic, high-quality spare parts for classic vehicles.', img: vintage },
   //{ id: 5, title: 'Games', description: 'Small Works', img: games, link: 'https://games-rho-pearl.vercel.app/'  }, 
 ];
 
