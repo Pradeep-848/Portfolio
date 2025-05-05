fd2b86f7dc0972eed3c3fdcf3f1e48ef9f079cf2
Pradeep M <125256248+Pradeep-848@users.noreply.github.com>
2025-05-05 15:23:47 +0530

    Update Tools.jsx

--- src/pages/Tools.jsx
+++ src/pages/Tools.jsx
@@ -7,6 +7,8 @@ import Marquee from 'react-fast-marquee'; // Import react-fast-marquee
 const vscode = images.values.vscode;
 const expo = images.values.expo;
 const antdesign = images.values.antdesign;
+const shadcn = images.values.shadcn;
+const tailwind = images.values.tailwind;
 const bootstrap = images.values.bootstrap;
 const git = images.values.git;
 const sql = images.values.sql;
@@ -20,11 +22,13 @@ const toolsData = [
   { id: 2, title: 'Expo', img: expo },
   { id: 3, title: 'AntDesign', img: antdesign },
   { id: 4, title: 'Bootstrap', img: bootstrap },
-  { id: 5, title: 'Git', img: git },
-  { id: 6, title: 'MsSql', img: sql },
-  { id: 7, title: 'Spring', img: spring },
-  { id: 8, title: 'Postman', img: postman },
-  { id: 9, title: 'Vercel', img: vercel },
+  { id: 5, title: 'ShadCN UI', img: shadcn },
+  { id: 6, title: 'Tailwind CSS', img: tailwind },
+  { id: 7, title: 'Git', img: git },
+  { id: 8, title: 'MsSql', img: sql },
+  { id: 9, title: 'Spring', img: spring },
+  { id: 10, title: 'Postman', img: postman },
+  { id: 11, title: 'Vercel', img: vercel },
 ];
 
 function Tools() {
